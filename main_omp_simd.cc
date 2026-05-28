f3342916a99c5f525b68cae4b42df49dfd0456cc
Your Name <you@example.com>
2026-05-28 15:55:40 +0300

    replaced c++20 std:format

--- main_omp_simd.cc
+++ main_omp_simd.cc
@@ -5,6 +5,7 @@
 #include <getopt.h>
 #include <iostream>
 #include <omp.h>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -14,6 +15,7 @@
 #include <format>
 #include <hwy/contrib/math/math-inl.h>
 #include <hwy/highway.h>
+#include <sstream>
 
 enum class TargetFunction
 {
@@ -179,7 +181,6 @@ f_gaussian_scalar (double h, double k)
   return std::exp (-(hk * hk));
 }
 
-
 /**
  * @brief Evaluates an unwindowed, signed power-law decay curve with static hardware safety cutoffs.
  *
@@ -226,7 +227,6 @@ f_extremal_candidate_scalar (double h, double k, double p)
   double res = std::pow (abs_hk, -1.0 / p);
   return (hk < 0.0) ? -res : res;
 }
-
 
 /**
  * @brief Calculates a specialized logarithmic scaling factor with domain boundaries.
@@ -277,7 +277,6 @@ f_extremal_scalar (double h, double k, double pi)
   double log_arg = (std::sqrt (tmp) + 1.0) / res_denom;
   return (2.0 / pi) * std::log (log_arg);
 }
-
 
 /**
  * @brief Evaluates the discrete kernel for a modified or truncated Hilbert transform.
@@ -744,8 +743,13 @@ main (int argc, char **argv)
       }
   }
 
-  std::string   csv_file = std::format ("out_p-{}_win-{}_func-{}_iter-{}.csv", PP, L, FUN, MAX);
-  std::ofstream file (csv_file);
+  std::stringstream ss;
+  ss << "out_p-" << PP
+     << "_win-" << L
+     << "_func-" << FUN
+     << "_iter-" << MAX << ".csv";
+
+  std::ofstream file (ss.str ());
 
   // Sequential Print step to guarantee sorted logs
   for (long i = 1; i < MAX; i++)
@@ -760,7 +764,7 @@ main (int argc, char **argv)
                        results_buffer[i].grafakos);
         }
     }
-
+  file.close ();
   std::cout << "Done! " << std::endl;
 
   return 0;
